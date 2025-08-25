5ec6f38c629583c232a0afacd4a3917d78458e50
Fankekeke <1258846421@qq.com>
2025-08-25 19:49:11 +0800

    fix - 派修功能修改

--- frontend/src/views/manage/repair/RepairEdit.vue
+++ frontend/src/views/manage/repair/RepairEdit.vue
@@ -133,7 +133,7 @@
       </a-row>
       <br/>
       <br/>
-      <a-row style="padding-left: 24px;padding-right: 24px;" :gutter="15" v-if="repairData.worker == null && repairData.repairStatus == 1">
+      <a-row style="padding-left: 24px;padding-right: 24px;" :gutter="15" v-if="repairData.worker == null">
         <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">设置维修员工</span></a-col>
         <a-col :span="10">
           <a-select v-model="workerId" style="width: 100%">
